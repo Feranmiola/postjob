18f0cad98dbbb3d4cae4822e6ad398480e373d17
feranmiola <osunjuyigbeiyin@gmail.com>
2025-05-24 18:25:35 +0100

    Refactor Home component to enhance structure and readability by removing unused code. Update layout and page components to implement a consistent dark background color.

---
+++ src/components/Shared/Topbar.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import PostJobIcon from "../Icons/PostJobIcon";
+
+const Topbar = () => {
+  return (
+    <div className="bg-[#161815] w-screen px-10 py-5 flex flex-row items-center justify-between ">
+      <PostJobIcon />
+    </div>
+  );
+};
+
+export default Topbar;
