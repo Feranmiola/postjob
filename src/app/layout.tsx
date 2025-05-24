1d45052d93c43e9ce087573b26619d05345730cd
feranmiola <osunjuyigbeiyin@gmail.com>
2025-05-24 18:23:16 +0100

    Update layout and page components to apply a dark background color. Removed unused code from the Home component for a cleaner structure.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} bg-[#161815]`}>{children}</body>
     </html>
   );
 }
