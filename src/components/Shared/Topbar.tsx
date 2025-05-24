5b4c6ef986e1ff783492b14b0646a269bc68b8d1
feranmiola <osunjuyigbeiyin@gmail.com>
2025-05-24 18:28:43 +0100

    Enhance Topbar component by adding an AI Cover Letter Generator link and improving layout structure for better alignment and spacing.

--- src/components/Shared/Topbar.tsx
+++ src/components/Shared/Topbar.tsx
@@ -4,7 +4,15 @@ import PostJobIcon from "../Icons/PostJobIcon";
 const Topbar = () => {
   return (
     <div className="bg-[#161815] w-screen px-10 py-5 flex flex-row items-center justify-between ">
-      <PostJobIcon />
+      <div className="flex flex-row  items-center space-x-3">
+        <PostJobIcon />
+
+        <div className="flex flex-row items-center space-x-4">
+          <p className="text-base text-[#909090] cursor-pointer hover:text-white transition-all ease-in-out font-semibold">
+            AI Cover Letter Generator
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
