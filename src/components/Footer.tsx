a10e67a0a742e04d9c79c0ceff5737b0a4416494
feranmiola <osunjuyigbeiyin@gmail.com>
2025-05-26 19:26:57 +0100

    Enhance Footer component by adding new navigation links for platform features, improving user accessibility and engagement.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -10,8 +10,34 @@ const Footer = () => {
           keyword checker, outreach message writer, and more. Powered by GPT
         </p>
 
-        <div className="h-[44px] w-max px-6 rounded-[12px] bg-transparent hover:bg-dark hover:text-green transition-all ease-in-out cursor-pointer border border-dark text-dark flex items-center justify-center text-base font-semibold">
+        <div className="h-[44px] px-6 rounded-[12px] bg-transparent hover:bg-dark hover:text-green transition-all ease-in-out cursor-pointer w-max border border-dark text-dark flex items-center justify-center text-base font-semibold">
           Try for free
+        </div>
+      </div>
+
+      <div className="w-[1126px] flex flex-col justify-between">
+        <div className="flex flex-col space-y-3">
+          <p className="text-xl font-semibold text-black">Platform</p>
+          <div className="flex flex-col space-y-2">
+            <p className="text-black cursor-pointer w-max hover:font-semibold transition-all ease-in-out text-base">
+              Plans and Prices
+            </p>
+            <p className="text-black cursor-pointer w-max hover:font-semibold transition-all ease-in-out text-base">
+              AI Create Letters
+            </p>
+            <p className="text-black cursor-pointer w-max hover:font-semibold transition-all ease-in-out text-base">
+              AI Resume Checker
+            </p>
+            <p className="text-black cursor-pointer w-max hover:font-semibold transition-all ease-in-out text-base">
+              Resume Templates
+            </p>
+            <p className="text-black cursor-pointer w-max hover:font-semibold transition-all ease-in-out text-base">
+              Sell Templates
+            </p>
+            <p className="text-black cursor-pointer w-max hover:font-semibold transition-all ease-in-out text-base">
+              Blog
+            </p>
+          </div>
         </div>
       </div>
     </div>
