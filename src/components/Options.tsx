c82719a895900a4e2eb5874e504e31d6c8a92187
feranmiola <osunjuyigbeiyin@gmail.com>
2025-05-28 22:33:07 +0100

    Update Options component to replace AI cover letter generator content with resume optimization features, enhancing clarity and user engagement.

--- src/components/Options.tsx
+++ src/components/Options.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Option1 from "./Icons/Option1";
+import Option2 from "./Icons/Option2";
 
 const Options = () => {
   return (
@@ -22,19 +23,19 @@ const Options = () => {
       </div>
 
       <div className="flex flex-row items-center space-x-5">
-        <Option1 />
+        <Option2 />
 
         <div className="flex flex-col space-y-3 w-[456px]">
           <p className="font-semibold leading-tight text-[48px] text-black">
-            Free AI cover letter generator powered by GPT
+            Optimize the keywords in your resume
           </p>
           <p className="text-black text-base">
-            A free AI cover letter generator powered by GPT is a tool that uses
-            artificial intelligence and natural language processing to help job
-            seekers create customized and effective cover letters.
+            By optimizing keywords in your resume, you can increase your chances
+            of getting noticed by recruiters and landing interviews for the jobs
+            you want.
           </p>
           <div className="h-[44px] px-6 rounded-[12px] bg-transparent hover:bg-dark hover:text-green transition-all ease-in-out cursor-pointer w-max border border-dark text-dark flex items-center justify-center text-base font-semibold">
-            Try For free Now
+            Optimize your Resume
           </div>
         </div>
       </div>
